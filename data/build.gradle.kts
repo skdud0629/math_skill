cf94c192a7043247b03279cddfe42edb348af890
skdud0629 <skdud4081@gmail.com>
2025-03-10 16:26:31 +0900

    Add: data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,26 +1,22 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.refactoring_android.math_skill"
+    namespace = "com.refactoring_android.math_skill.data"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.refactoring_android.math_skill"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 10001
-        versionName = "1.0.1"
-        //버전 관리
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -34,21 +30,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        viewBinding = true
-        dataBinding = true
-    }
 }
 
 dependencies {
     implementation(project(":domain"))
-    implementation(project(":data"))
-    implementation(project(":core"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,3 +23,4 @@ rootProject.name = "math_skill"
 include(":app")
 include(":core")
 include(":domain")
+include(":data")
